b59e692a80da7e6b5e3bd8ca4da0a057ba1d4f63
Samuel Meuli <me@samuelmeuli.com>
2019-10-08 16:41:29 +0200

    Release `tmignore` v0.2.0

--- Formula/tmignore.rb
+++ Formula/tmignore.rb
@@ -2,8 +2,8 @@ class Tmignore < Formula
 	desc "Exclude development files from Time Machine backups"
 	homepage "https://github.com/samuelmeuli/tmignore"
 	url "https://github.com/samuelmeuli/tmignore.git",
-		:tag => "v0.1.0",
-		:revision => "e6242c5892786797157fc1fdfc93346d86021dd8"
+		:tag => "v0.2.0",
+		:revision => "93bdc18b6d13ee9866d0adfb2a73f7a3f8879620"
 	head "https://github.com/samuelmeuli/tmignore.git"
 
 	depends_on :xcode => ["10.0", :build]
