81e1216d22e72200733195ed522213e33ba9dff1
Samuel Meuli <me@samuelmeuli.com>
2020-04-29 14:23:50 +0200

    Release `tmignore` v1.2.0

--- Formula/tmignore.rb
+++ Formula/tmignore.rb
@@ -1,15 +1,16 @@
 class Tmignore < Formula
 	desc "Exclude development files from Time Machine backups"
 	homepage "https://github.com/samuelmeuli/tmignore"
-	url "https://github.com/samuelmeuli/tmignore/archive/v1.1.2.tar.gz"
-	sha256 "df105e301a7b3f6f37025fb6674e67b9246fdd89f2dcb7ceb3862995bfb5483b"
+	url "https://github.com/samuelmeuli/tmignore/archive/v1.2.0.tar.gz"
+	sha256 "1fdfd3da9cd5e1b73ee98a9498146405c39e98d43c1c9b95efc923faa8a65e24"
 	head "https://github.com/samuelmeuli/tmignore.git"
 
-	depends_on :xcode => ["10.0", :build]
+	depends_on :macos => :high_sierra
+	depends_on :xcode => :build
 
 	def install
 		system "make", "build"
-		bin.install "./dist/tmignore"
+		bin.install "./bin/tmignore"
 		system "cp", "./homebrew.tmignore.plist", prefix
 	end
 
